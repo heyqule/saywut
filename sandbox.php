363234256fde6927943fc97419d64129bacd4996
heyqule <hq.jfax@gmail.com>
2013-10-19 15:03:19 -0400

    Commit sandbox before major change..

--- sandbox.php
+++ sandbox.php
@@ -8,7 +8,9 @@
 error_reporting(E_ALL | E_STRICT);
 ini_set('display_errors',1);
 
-// require_once('bots/Twitter_Bot.php');
+require_once('config.php');
+require_once('bots/Raw_Bot.php');
+/*
 require_once('bots/libs/weibo/WeiboOAuthV2.php');
 
 $appKey = '1191803748';
@@ -17,25 +19,63 @@ $oauthKey = '2.00UwBTuBGbgeSB71e24d0cdf0_uV25';
 
 $connection = new WeiboOAuthV2($appKey,$appSecret,$oauthKey);
 
-$redirect_uri = "http://www.heyqule.com/saywut/sandbox.php";
+*/
+echo '---------------------------------<br />';
+echo 'LOAD TEST<br />';
+echo '---------------------------------<br /><br />';
 
-if(!empty($_REQUEST['code']))
-{
-    $keys = array();
-    $keys['code'] = $_REQUEST['code'];
-    $keys['redirect_uri'] = $redirect_uri;
-    try {
-        $token = $connection->getAccessToken( 'code', $keys ) ;
-        print_r($token);
-    } catch (OAuthException $e) {
-    }
-}
-else
-{
-    $keys = array(
-        'redirect_uri' => $redirect_uri
-    );
-    echo '<a href="'.$connection->getAuthorizeURL($redirect_uri).'">Login</a>';
-}
+$post = new Post();
 
+$post->load(205);
+
+print_r($post);
+
+echo '<br /><br />';
+echo '----------------------------------<br />';
+echo 'Save Test<br />';
+echo '----------------------------------<br /><br />';
+$post->title="123456";
+$post->save();
+
+$post2 = new Post();
+
+$post2->load(205);
+
+echo $post2->title;
+
+$post2->title = '';
+$post2->id = null;
+$post2->save();
+
+echo '<br /><br />';
+echo '----------------------------------<br />';
+echo 'Delete Test<br />';
+echo '----------------------------------<br /><br />';
+
+$post2->delete();
+
+$post3 = new Post();
+$post3->load(205);
+echo "TTT".$post3->title;
+
+$post2->save();
+
+echo '<br /><br />';
+echo '----------------------------------<br />';
+echo 'Collection Test<br />';
+echo '----------------------------------<br /><br />';
+
+$post_collector = new Post_Collection();
+$post_collector->addMetaWhere('hidden','=',1);
+$posts = $post_collector->loadByQuery(0,10);
+
+print_r($posts);
+
+echo '<br /><br />';
+echo '----------------------------------<br />';
+echo 'Core::getBotKey:'.Core::getBotKey($GLOBALS['BOT_CONFIG'][1]).' - '.Core::getBotKey($GLOBALS['BOT_CONFIG'][12]).'<br />';
+echo '-----------------------------------<br /><br />';
+
+
+echo 'MEMORY:'.memory_get_usage();
 ?>
