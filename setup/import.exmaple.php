cb1cd05c93ba2e1493d8a020aad3564429234889
heyqule <hq.jfax@gmail.com>
2013-10-19 15:16:38 -0400

    0.5 update

---
+++ setup/import.exmaple.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: heyqule
+ * Date: 10/16/13
+ * Time: 11:32 PM
+ * To change this template use File | Settings | File Templates.
+ */
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_error',1);
+
+
+require_once '../config.php';
+require_once ROOT_PATH.DS.'includes'.DS.'Post_Collection.php';
+
+
+try
+{
+
+$mysql = new PDO('mysql:host='.MYSQL_DB_HOST.';port='.MYSQL_DB_PORT.';dbname='.MYSQL_DB_NAME.';',MYSQL_DB_USER,MYSQL_DB_PASS);
+
+$bot_insert = "INSERT IGNORE INTO `posts_bots` (id, class, name) VALUES (:id, :class, :name);";
+
+$bot_config_exec = $mysql->prepare($bot_insert);
+
+echo 'begin<br />';
+$mysql->beginTransaction();
+
+foreach($GLOBALS['BOT_CONFIG'] as $key => $bot) {
+    if($key != 0)
+    {
+        $bot_config_exec->execute(array(
+           ':id'=>$key,
+           ':class'=>$bot['class'],
+           ':name'=>$bot['name']
+        ));
+    }
+}
+$mysql->commit();
+
+define('DB_PATH', 'something.sqlite');
+define('DB_USER','something');
+define('DB_PASS','something');
+
+$sqlite = new PDO('sqlite:'.ROOT_PATH.DS.DB_PATH,DB_USER,DB_PASS);
+
+$sth = $sqlite->prepare('SELECT * FROM posts;');
+$sth->execute();
+$rows = $sth->fetchAll();
+
+$posts = array();
+
+foreach($rows as $row) {
+    $post = new Post();
+    $post->id = $row['id'];
+    $post->title = $row['title'];
+    $post->contents = $row['contents'];
+    $post->provider_id = $row['provider_id'];
+    $post->provider_cid = $row['provider_cid'];
+    $post->tags = $row['tags'];
+    $post->custom_data = $row['custom_data'];
+    $post->time = $row['time'];
+    $post->hidden = $row['hidden'];
+    $post->update_time = $row['update_time'];
+    $posts[$post->id] = $post;
+}
+
+$post_insert = '
+INSERT INTO `posts`
+ (`id`, `title`, `provider_id`, `provider_cid`, `contents`, `create_time`, `update_time`) VALUES
+ (:id, :title, :provider_id, :provider_cid, :contents, :create_time, :update_time);
+';
+
+$meta_insert = '
+INSERT INTO `posts_meta` (`post_id`,`meta_name`,`meta_value`) VALUES (:post_id,:meta_name,:meta_value);
+';
+
+$post_exec = $mysql->prepare($post_insert);
+$meta_exec = $mysql->prepare($meta_insert);
+
+$mysql->beginTransaction();
+foreach($posts as $post) {
+
+    if(empty($post->provider_cid)) {
+        $post->provider_cid = uniqid();
+    }
+
+    $post_exec->execute(array(
+        ':id'=>null,
+        ':title'=>$post->title,
+        ':provider_id'=>$post->provider_id,
+        ':provider_cid'=>$post->provider_cid,
+        ':contents'=>$post->contents,
+        ':create_time'=>$post->time,
+        ':update_time'=>$post->update_time
+    ));
+    $id = $mysql->lastInsertId();
+
+    $meta_exec->execute(array(
+        ':post_id'=>$id,
+        ':meta_name'=>'hidden',
+        ':meta_value'=>$post->hidden
+    ));
+
+    $custom_data = json_decode($post->custom_data);
+
+    foreach($custom_data as $key => $value) {
+        $meta_exec->execute(array(
+            ':post_id'=>$id,
+            ':meta_name'=>$key,
+            ':meta_value'=>$value
+        ));
+    }
+
+
+    echo $post->id.' added to '.$id.'<br />';
+    print_r($post_exec->errorInfo());
+    echo '<br /><br />';
+}
+$mysql->commit();
+
+} catch(Exception $e) {
+    $mysql->rollBack();
+    echo '<br />ERROR:<br />';
+    echo nl2br(print_r($e,true));
+}
---
+++ setup/install.php
@@ -0,0 +1,91 @@
+<?php
+
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_error',1); 
+
+require_once '../config.php';
+
+try
+{
+    echo 'Initializing DB... <br />';
+    $mysql = new PDO('mysql:host='.MYSQL_DB_HOST.';port='.MYSQL_DB_PORT.';dbname='.MYSQL_DB_NAME.';',MYSQL_DB_USER,MYSQL_DB_PASS);
+
+    $mysql->exec('SET FOREIGN_KEY_CHECKS=0;');
+
+    echo 'Configuraing Database <br />';
+    $mysql->exec('
+DROP TABLE IF EXISTS `posts_bots`;
+
+CREATE TABLE IF NOT EXISTS `posts_bots` (
+  `id` int(11) unsigned AUTO_INCREMENT,
+  `class` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `POST_CLASS_COMBO` (`class`,`name`),
+  KEY `POST_CLASS` (`class`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+');
+
+$mysql->exec('
+DROP TABLE IF EXISTS `posts`;
+
+CREATE TABLE IF NOT EXISTS `posts` (
+  `id` bigint(20) unsigned AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `provider_id` int(10) unsigned DEFAULT NULL,
+  `provider_cid` varchar(255) DEFAULT NULL,
+  `contents` longtext NOT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `POST_EXTERNAL_PROVIDER_INDEX` (`provider_id`,`provider_cid`) COMMENT \'Post from external provider must be unique\',
+  KEY `POST_UPDATE_TIME` (`update_time`),
+  KEY `POST_CREATE_TIME` (`create_time`),
+  KEY `POST_EXTERNAL_PROVIDER_ID` (`provider_id`),
+  CONSTRAINT `POST_BOT` FOREIGN KEY (`provider_id`) REFERENCES `posts_bots` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+');
+
+$mysql->exec('
+DROP TABLE IF EXISTS `posts_meta`;
+
+CREATE TABLE IF NOT EXISTS `posts_meta` (
+  `id` bigint(20) unsigned AUTO_INCREMENT,
+  `post_id` bigint(20) unsigned NOT NULL,
+  `meta_name` varchar(255) DEFAULT NULL,
+  `meta_value` varchar(10240) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `POST_META_VARIABLE` (`post_id`,`meta_name`),
+  KEY `POST_META_VARIABLE_NAME` (`meta_name`),
+  CONSTRAINT `POST_META_POST_ID` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+');
+
+$mysql->exec('
+DROP TABLE IF EXISTS `posts_logs`;
+
+CREATE TABLE IF NOT EXISTS `posts_logs` (
+  `id` bigint(20) unsigned AUTO_INCREMENT,
+  `bot_id` int(11) unsigned NOT NULL,
+  `event_type` smallint(6) unsigned NOT NULL,
+  `message` text NOT NULL,
+  `create_time` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `LOG_EVENT_TYPE` (`event_type`),
+  KEY `LOG_CREATE_TIME` (`create_time`),
+  CONSTRAINT `LOG_POST_BOT` FOREIGN KEY (`bot_id`) REFERENCES `posts_bots` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+');
+
+$mysql->exec('
+SET FOREIGN_KEY_CHECKS=1;
+');
+
+echo 'Done...';
+
+
+
+} catch(Exception $e) {
+    echo '<br />ERROR:<br />';
+    echo nl2br(print_r($e,true));
+}
